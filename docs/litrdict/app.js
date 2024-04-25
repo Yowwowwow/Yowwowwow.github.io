25660ff72da93b0f90fc63b45ce68d1ede5c1569
Yowwowwow <76942268+Yowwowwow@users.noreply.github.com>
2024-04-25 23:27:51 +0800

    Initial commit of parseltp()

--- docs/litrdict/app.js
+++ docs/litrdict/app.js
@@ -22,7 +22,16 @@ const reps = { //replacements
     "=":"p_sa.svg",
     "6":"ping.svg",
     "^R":"q_wo.svg",
-    "8":"sye.svg"
+    "8":"sye.svg",
+
+    "v(":"v(coda).png",
+    "ye(":"ye(variant).png",
+    "lowerlip":"抿下脣.png",
+    "endtrill":"止彈.png",
+
+    "upperlip":"抿上脣.jpg",
+    "bothlips":"抿雙脣.jpg",
+    "fliplips":"翻脣.jpg"
 };
 const svgs = [
     "a", "ai", "an", "ang", "au",
@@ -47,3 +56,48 @@ const svgs = [
     "x",
     "ye_a", "yen", "yo", "yu", "yy"
 ];
+const pngs = [
+    "am", "ao",
+    "cg", "ck",
+    "ei", "em", "eu",
+    "fp",
+    "nh",
+    "oi", "om", "on",
+    "qh", "qk",
+    "we", "wf", "wm", "wo", "wu",
+    "yem", "yeng", "yeu", "yi",
+    "zb", "zs", "zz"
+];
+const jpgs = [
+    "cn", "cr",
+    "db",
+    "kh",
+    "lg", "lr",
+    "nm",
+    "q",
+    "tp",
+    "vb",
+    "wa", "wb", "wp",
+    "zf", "zl", "zn", "zp", "zr", "zv", "zwf"
+];
+function parseltp(){
+    const ltps = document.getElementsByClassName("ltp");
+    const height = "24px";
+    for(let fieldcount=0;fieldcount<ltps.length;fieldcount++){
+        let s = ltps[fieldcount].innerHTML;
+        const seps = s.split(';');
+        let ans = "";
+        for(let i=0;i<seps.length;i++){
+            if(seps[i] in reps){
+                let tmp = reps[seps[i]].slice(reps[seps[i]].length-3).concat("s");
+                ans = ans.concat(`<img src="${tmp}/${reps[seps[i]]}" height="${height}">`);
+            }
+            else if(svgs.includes(seps[i]))ans = ans.concat(`<img src="svgs/${seps[i]}.svg" height="${height}">`);
+            else if(pngs.includes(seps[i]))ans = ans.concat(`<img src="pngs/${seps[i]}.png" height="${height}">`);
+            else if(jpgs.includes(seps[i]))ans = ans.concat(`<img src="jpgs/${seps[i]}.jpg" height="${height}">`);
+            else ans = ans.concat(seps[i]);
+        }
+        ltps[fieldcount].innerHTML = ans;
+    }
+}
+parseltp();
