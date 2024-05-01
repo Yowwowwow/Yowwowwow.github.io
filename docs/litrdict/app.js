5b84972648bc45ea5487d332c4eab438c724d0d9
Yowwowwow <76942268+Yowwowwow@users.noreply.github.com>
2024-05-02 00:23:21 +0800

    Edit test

--- docs/litrdict/app.js
+++ docs/litrdict/app.js
@@ -1,7 +1,23 @@
 function getEntry(){
     const s = window.location.href;
-    return s.includes('?')?decodeURI(s.substring(s.indexOf('?')+1, s.includes('&')?s.indexOf('&'):s.length)):"";
+    if(!s.includes('?') || (s.includes('#') && s.indexOf('#')<s.indexOf('?')))return "";
+    let end = s.length;
+    if(s.includes('&'))end = s.indexOf('&');
+    if(s.includes('#') && s.indexOf('#')<end)end = s.indexOf('#');
+    return decodeURI(s.substring(s.indexOf('?')+1, end));
 }
 function getTesttxt(func){
     fetch("test.txt").then(x => x.text()).then(y => func(y));
+}
+function setEntry(w){
+    const s = window.location.href;
+    let start = s.length;
+    if(s.includes('?'))start = s.indexOf('?');
+    if(s.includes('#') && s.indexOf('#')+1<start)start = s.indexOf('#');
+    let end = s.length;
+    if(s.includes('&'))end = s.indexOf('&');
+    if(s.includes('#') && s.indexOf('#')<end)end = s.indexOf('#');
+    let head = s.substring(0, start);
+    let tail = s.substring(end, s.length);
+    history.pushState({}, "", head.concat("?", w, tail));
 }
